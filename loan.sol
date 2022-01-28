3bc2c2f38152d3859fa62f0959c1406191ed2951
Aron113 <93630749+Aron113@users.noreply.github.com>
2022-01-28 21:23:52 +0800

    Update loan.sol

--- loan.sol
+++ loan.sol
@@ -7,6 +7,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
     }
 interface chainlink{                                                                                    //Define chainlink token's interface
     function approve(address spender, uint256 amount) external returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);   //Use the transferFrom() function 
 }
     
@@ -51,14 +52,13 @@ contract Liquefi{
 
     function depositChain( uint256 amount) public{               //Deposit  Chainlink tokens into the smartcontract
         chainlink(0x01BE23585060835E02B77ef475b0Cc51aA1e0709).transferFrom(msg.sender,address(this),amount);   //Chainlink tokens transferred from user to the contract
-         currentBalance=currentBalance + amount;
+         currentBalance=currentBalance + amount;                  //New currentBalance calculated
     }
 
     function withdrawChain (uint256 amount) public{
         require(msg.sender==user,'Not a user!');
         require(amount<=currentBalance);                  //Withdraw amount must be <= current Balance
-        currentBalance=currentBalance-amount;
-        chainlink(0x01BE23585060835E02B77ef475b0Cc51aA1e0709).approve(user, amount);
-        chainlink(0x01BE23585060835E02B77ef475b0Cc51aA1e0709).transferFrom(address(this),user,amount);   
+        currentBalance=currentBalance-amount;              //New currentBalance calculated
+        chainlink(0x01BE23585060835E02B77ef475b0Cc51aA1e0709).transfer(user,amount);    //Chainlink tokens withdrawn to user's account
     }
 }
