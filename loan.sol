3b481cc147bca7bcf28af57e3cff7312b09a5c69
Aron113 <93630749+Aron113@users.noreply.github.com>
2022-01-28 16:44:54 +0800

    Update loan.sol

--- loan.sol
+++ loan.sol
@@ -1,33 +1,56 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.7.0 <0.9.0;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract Liquefi{
+ interface IlendingPool{                                  //Define lending pool's interface
+        function repay(uint amount) payable external;     //Use lending pool's repay() function
+    }
     
-    constructor(address lendingPool) {
-     lendingpool=lendingPool;
-     user=msg.sender;
+contract Liquefi{
+   
+    constructor(address lendingPool) {                  //Enter in lending pool's address
+     lendingpool=lendingPool;                   
+     user=msg.sender;                                    //Creator of contract
+     
     
     }
     address public lendingpool;
     uint public currentBalance=0;
     address public user;
-    uint public priceSet;
-    uint public realPrice;
-
-    function deposit() public payable{
+    int public priceSet;
+    
+    function deposit() public payable{                      //Deposit funds into contract
         require(msg.sender==user);
         currentBalance=currentBalance + msg.value;
     }
 
-    function withDraw(uint amountWithdraw) public {
+    function withDraw(uint amountWithdraw) public {         //Withdraw balance      
         require(msg.sender==user,'Not a user!');
-        require(amountWithdraw<=currentBalance);
+        require(amountWithdraw<=currentBalance);            //Withdraw amount must be <= current Balance
         payable(msg.sender).call{value:amountWithdraw}('');
         currentBalance=currentBalance-amountWithdraw;
     }
-    function setPrice(uint setprice) public {
+
+    function setPrice(int setprice) public {                //Set repayment price
+        require(msg.sender==user,'Not a user!');
         priceSet=setprice;
     }
-  
+
+    function repayLoan() public{                            //Function to be called continuously by relayer
+        require(priceSet>=getLatestPrice());                //priceSet >= realPrice for our contract to call the lending pool's repay() function
+        IlendingPool(lendingpool).repay(currentBalance);
+        //current balance=0?
+    
+    }
+
+    function getLatestPrice() public view returns (int) {     //Get latest price from Chainlink data feed      
+        (
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) =  AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e).latestRoundData(); // ETH/USD PRICE FEED
+        return price/10**8; //8 decimals
+    }
 }
