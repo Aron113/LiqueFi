6792fcdf9b7796c146c4d104c5a7f0a2a3f566c2
Aron113 <93630749+Aron113@users.noreply.github.com>
2022-01-30 17:19:12 +0800

    Update loan.sol

--- loan.sol
+++ loan.sol
@@ -2,10 +2,11 @@
 pragma solidity >=0.7.0 <0.9.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
- interface IlendingPool{                                  //Define lending pool's interface
-        function repay(uint amount) external;             //Use lending pool's repay() function
-    }
-interface BUSD{                                                                                    //Define Kovan BUSD's interface
+ interface IlendingPool{                                                                                                        //Define lending pool's interface
+     function repay(address asset, uint256 amount, uint256 rateMode, address onBehalfOf) external returns (uint256);             //Use lending pool's repay() function  
+     }
+    
+interface ERC20{                                                                                    //Define ERC20's interface
     function approve(address spender, uint256 amount) external returns (bool);
     function transfer(address recipient, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);   //Use the transferFrom() function 
@@ -13,12 +14,16 @@ interface BUSD{                                                                 
     
 contract Liquefi{
    
-    constructor(address lendingPool) {                  //Enter in lending pool's address
-     lendingpool=lendingPool;                   
+    constructor(address lendingpool, address tokenaddress,uint ratemode) {                  //Enter in lending pool's address
+     lendingPool=lendingpool;    
+     tokenAddress=tokenaddress;
+     rateMode=ratemode;
      user=msg.sender;                                    //Creator of contract
     }
 
-    address public lendingpool;
+    address public lendingPool;
+    address public tokenAddress;
+    uint public rateMode;
     uint public currentBalance=0;
     address public user;
     int public priceSet;
@@ -32,8 +37,8 @@ contract Liquefi{
     function repayLoan() public {     //Function to be called continuously by relayer                            
         require(priceSet>=getLatestPrice());               //priceSet >= realPrice for our contract to call the lending pool's repay() function
         require(currentBalance!=0);
-        BUSD(0x4c6E1EFC12FDfD568186b7BAEc0A43fFfb4bCcCf).approve(lendingpool,currentBalance);
-        IlendingPool(lendingpool).repay(currentBalance);
+        ERC20(tokenAddress).approve(lendingpool,currentBalance);
+        IlendingPool(lendingPool).repay(tokenAddress,currentBalance,rateMode,user);
         currentBalance=0;
        
     }
@@ -49,15 +54,15 @@ contract Liquefi{
         return price/10*8; //8 decimals
     }
 
-    function depositChain( uint256 amount) public{               //Deposit  Kovan BUSD into the smartcontract
-        BUSD(0x4c6E1EFC12FDfD568186b7BAEc0A43fFfb4bCcCf).transferFrom(msg.sender,address(this),amount);   //Kovan BUSD transferred from user to the contract
+    function depositERC20( uint256 amount) public{               //Deposit ERC20 into the smartcontract
+        ERC20(tokenAddress).transferFrom(msg.sender,address(this),amount);   //ERC20 transferred from user to the contract
          currentBalance=currentBalance + amount;                  //New currentBalance calculated
     }
 
-    function withdrawChain (uint256 amount) public{
+    function withdrawERC20(uint256 amount) public{
         require(msg.sender==user,'Not a user!');
         require(amount<=currentBalance);                  //Withdraw amount must be <= current Balance
         currentBalance=currentBalance-amount;              //New currentBalance calculated
-        BUSD(0x4c6E1EFC12FDfD568186b7BAEc0A43fFfb4bCcCf).transfer(user,amount);    //Kovan BUSD tokens withdrawn to user's account
+        ERC20(tokenAddress).transfer(user,amount);    //ERC20 tokens withdrawn to user's account
     }
 }
