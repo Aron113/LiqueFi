2c22290182d30ebcc39c88a5f9c3d0036e7ccb09
Aron113 <93630749+Aron113@users.noreply.github.com>
2022-01-30 16:28:30 +0800

    Update loan.sol

--- loan.sol
+++ loan.sol
@@ -5,7 +5,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
  interface IlendingPool{                                  //Define lending pool's interface
         function repay(uint amount) external;             //Use lending pool's repay() function
     }
-interface chainlink{                                                                                    //Define chainlink token's interface
+interface BUSD{                                                                                    //Define Kovan BUSD's interface
     function approve(address spender, uint256 amount) external returns (bool);
     function transfer(address recipient, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);   //Use the transferFrom() function 
@@ -32,7 +32,7 @@ contract Liquefi{
     function repayLoan() public {     //Function to be called continuously by relayer                            
         require(priceSet>=getLatestPrice());               //priceSet >= realPrice for our contract to call the lending pool's repay() function
         require(currentBalance!=0);
-        chainlink(0x01BE23585060835E02B77ef475b0Cc51aA1e0709).approve(lendingpool,currentBalance);
+        BUSD(0x4c6E1EFC12FDfD568186b7BAEc0A43fFfb4bCcCf).approve(lendingpool,currentBalance);
         IlendingPool(lendingpool).repay(currentBalance);
         currentBalance=0;
        
@@ -45,12 +45,12 @@ contract Liquefi{
             uint startedAt,
             uint timeStamp,
             uint80 answeredInRound
-        ) =  AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e).latestRoundData(); // ETH/USD PRICE FEED
-        return price/10**8; //8 decimals
+        ) =  AggregatorV3Interface(0xbF7A18ea5DE0501f7559144e702b29c55b055CcB).latestRoundData(); // BUSD/ETH PRICE FEED
+        return 1/price/10**18; //18 decimals
     }
 
-    function depositChain( uint256 amount) public{               //Deposit  Chainlink tokens into the smartcontract
-        chainlink(0x01BE23585060835E02B77ef475b0Cc51aA1e0709).transferFrom(msg.sender,address(this),amount);   //Chainlink tokens transferred from user to the contract
+    function depositChain( uint256 amount) public{               //Deposit  Kovan BUSD into the smartcontract
+        BUSD(0x4c6E1EFC12FDfD568186b7BAEc0A43fFfb4bCcCf).transferFrom(msg.sender,address(this),amount);   //Kovan BUSD transferred from user to the contract
          currentBalance=currentBalance + amount;                  //New currentBalance calculated
     }
 
@@ -58,6 +58,6 @@ contract Liquefi{
         require(msg.sender==user,'Not a user!');
         require(amount<=currentBalance);                  //Withdraw amount must be <= current Balance
         currentBalance=currentBalance-amount;              //New currentBalance calculated
-        chainlink(0x01BE23585060835E02B77ef475b0Cc51aA1e0709).transfer(user,amount);    //Chainlink tokens withdrawn to user's account
+        BUSD(0x4c6E1EFC12FDfD568186b7BAEc0A43fFfb4bCcCf).transfer(user,amount);    //Kovan BUSD tokens withdrawn to user's account
     }
 }
