5a30b60f4a5d0bb749bf7b7a705604bd1d14628b
Aron113 <93630749+Aron113@users.noreply.github.com>
2022-02-01 16:28:04 +0800

    Update LiqueFi.sol

--- LiqueFi.sol
+++ LiqueFi.sol
@@ -4,8 +4,13 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";    
 
  interface IlendingPool{                                                                                                        //Define lending pool's interface
      function repay(address asset, uint256 amount, uint256 rateMode, address onBehalfOf) external returns (uint256);             //Use lending pool's repay() function  
-     }
-    
+}
+
+ interface FarmingPool{
+     function mint(uint mintAmount) external returns (uint);
+     function redeem(uint redeemTokens) external returns (uint);
+}    
+
 interface ERC20{                                                                                    //Define ERC20's interface
     function approve(address spender, uint256 amount) external returns (bool);
     function transfer(address recipient, uint256 amount) external returns (bool);
@@ -25,6 +30,7 @@ contract Liquefi{
     address public tokenAddress;
     uint public rateMode;
     uint public currentBalance=0;
+    uint public mintedAmount=0;
     address public user;
     int public priceSet;
     
@@ -32,15 +38,6 @@ contract Liquefi{
     function setPrice(int setprice) public {                //Set repayment price
         require(msg.sender==user,'Not a user!');
         priceSet=setprice;
-    }
-
-    function repayLoan() public {                         //Function to be called continuously by relayer                            
-        require(priceSet>=getLatestPrice());               //priceSet >= realPrice for our contract to call the lending pool's repay() function
-        require(currentBalance!=0);
-        ERC20(tokenAddress).approve(lendingPool,currentBalance);               //lendingPool address is approved to take the tokens from our smart contract
-        IlendingPool(lendingPool).repay(tokenAddress,currentBalance,rateMode,user);    //repay() function of lending pool contract is called
-        currentBalance=0;
-       
     }
 
     function getLatestPrice() public view returns (int) {     //Get latest price from Chainlink data feed      
@@ -64,5 +61,41 @@ contract Liquefi{
         require(amount<=currentBalance);                  //Withdraw amount must be <= current Balance
         currentBalance=currentBalance-amount;              //New currentBalance calculated
         ERC20(tokenAddress).transfer(user,amount);    //ERC20 tokens withdrawn to user's account
+    }
+    
+    function farm(uint mintAmount) public{
+        require(msg.sender==user,'Not a user!');
+        require(mintAmount<=currentBalance);
+        currentBalance=CurrentBalance-mintAmount;
+        mintedAmount=mintedAmount+mintAmount;
+        ERC20(tokenAddress).approve(0xf0d0eb522cfa50b716b3b1604c4f0fa6f04376ad,mintAmount);
+        FarmingPool(0xf0d0eb522cfa50b716b3b1604c4f0fa6f04376ad).mint(mintAmount);
+    }
+    
+    function redeemFromFarm(uint redeemAmount) public{
+        require(msg.sender==user,'Not a user!');
+        FarmingPool(0xf0d0eb522cfa50b716b3b1604c4f0fa6f04376ad).redeem(redeemAmount);
+        currentBalance=currentBalance+redeemAmount;
+        mintedAmount=mintedAmount-redeemAmount;
+    }
+    
+    function repayLoanfromFarm() public {                                    //Function to be called continuously by relayer   
+        require(priceSet>=getLatestPrice());               //priceSet >= realPrice for our contract to call the lending pool's repay() function
+        FarmingPool(0xf0d0eb522cfa50b716b3b1604c4f0fa6f04376ad).redeem(mintedAmount);
+        currentBalance=currentBalance+mintedAmount;
+        mintedAmount=0;
+        require(currentBalance!=0);
+        ERC20(tokenAddress).approve(lendingPool,currentBalance);
+        ERC20(tokenAddress).transfer(lendingPool,currentBalance);       //this will actly be the repay() function but for testing we used transfer
+        currentBalance=0;
+    }
+    
+     function repayLoanfromCurrentBalance() public {                         //Function to be called continuously by relayer                            
+        require(priceSet>=getLatestPrice());               //priceSet >= realPrice for our contract to call the lending pool's repay() function
+        require(currentBalance!=0);
+        ERC20(tokenAddress).approve(lendingPool,currentBalance);               //lendingPool address is approved to take the tokens from our smart contract
+        IlendingPool(lendingPool).repay(tokenAddress,currentBalance,rateMode,user);    //repay() function of lending pool contract is called
+        currentBalance=0;
+       
     }
 }
