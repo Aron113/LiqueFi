032192bbcc73d392217c8f6b0729d71b2db991e5
Aron113 <93630749+Aron113@users.noreply.github.com>
2022-02-02 21:15:29 +0800

    Update LiquefiFactory.sol

--- LiquefiFactory.sol
+++ LiquefiFactory.sol
@@ -4,15 +4,28 @@ import './Liquefi.sol';
 
 
 contract LiquefiFactory{
-    Liquefi[] public liqueFiList;     //Array of Liquefi smart contract addresses
+    
+    mapping(address=>userContracts) public UserContracts;             //Map each address(key) to a struct containing an array of their Liquefi contracts(value)
+    Liquefi[] public liqueFiList;                                    //Array of Liquefi smart contract addresses
+    
+    struct userContracts{
+        uint nil;                                                   //nil value
+        address[] usercontracts;                                     //Array of the Liquefi contracts
+    }
+
 
     function startLiqueFi(address lendingpool, address tokenaddress,uint ratemode) public {
         Liquefi newcontract = new Liquefi(lendingpool, tokenaddress, ratemode, address(this));
         liqueFiList.push(newcontract);
+        UserContracts[msg.sender].usercontracts.push(address(newcontract));
     }
 
-    function viewLiquefiContracts()public view returns (Liquefi[] memory){     //View an array of all the contracts
+    function viewLiquefiContracts()public view returns (Liquefi[] memory){     //View an array of ALL the contracts
         return liqueFiList;
+    }
+    
+    function viewusercontracts(address user)public view returns (address[] memory){         //View an array of a user's contracts
+        return UserContracts[user].usercontracts;
     }
   
 }
